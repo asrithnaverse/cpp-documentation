c555f0d1c06b6903ac6667c29abb0ba2714c28c9
asrithnaverse <asrithnaverse@gmail.com>
2025-06-19 13:15:24 +0530

    Create sixth.cpp

---
+++ Basics/sixth.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //string
+    string raju = "dog";
+    string neha ;
+    cin>> neha;
+    cout<< "raju is a " <<raju <<"\n" ;
+    cout<< "neha is a "<< neha <<endl;
+}
