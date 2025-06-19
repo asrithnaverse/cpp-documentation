bad7ca62ff0bef2a51892ad53371a0cb48f205d6
asrithnaverse <asrithnaverse@gmail.com>
2025-06-19 13:05:49 +0530

    Create fifth.cpp

---
+++ Basics/fifth.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //int
+    int x=5;
+    long y=1000000;
+    long long a=100000000000;
+    float b=2.305;
+    double c=3.14;
+    cout <<"the value of the operation a+b+c-x-y is =" << a+b+c-x-y;
+}
