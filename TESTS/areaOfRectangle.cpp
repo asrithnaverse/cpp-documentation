785ecba5096958a17a06ce150d745ae74432db74
asrithnaverse <asrithnaverse@gmail.com>
2025-06-22 16:42:08 +0530

    Create areaOfRectangle.cpp

---
+++ TESTS/areaOfRectangle.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    cout << "the length of rectangle is :"  ;
+    cin >> x ;
+    int y;
+    cout << "the breadth of the rectangle is :"  ;
+    cin >> y ;
+    cout << "the area of the rectangle is:" <<x*y <<"\n" ;
+    cout << "the perimeter of the rectangle is " <<2*(x+y) <<"\n" ;
+}
