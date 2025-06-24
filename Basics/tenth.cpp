4ff6208a85ff251bc6133bcb9a2515972a7fd41d
asrithnaverse <asrithnaverse@gmail.com>
2025-06-24 13:53:24 +0530

    Create tenth.cpp

---
+++ Basics/tenth.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    cout << "Your age is : " ;
+    cin >> x;
+    if(x >=18){
+cout << "Yes you are an adult" << "\n" ;
+}
+    else{
+        cout << "You are not an adult" ;
+}
+}
