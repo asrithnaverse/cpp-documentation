eccba370dae2488f1f43ff4bf2cc85f49af1094a
asrithnaverse <asrithnaverse@gmail.com>
2025-06-24 14:05:21 +0530

    Create eleventh.cpp

---
+++ Basics/eleventh.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x ;
+    cout << "Your age is : " ;
+    cin >> x ;
+    if(x >=13){
+        cout << "You are a teen";
+    }
+        else if(x <10){
+            cout << "You are a kid" ;
+}
+}
