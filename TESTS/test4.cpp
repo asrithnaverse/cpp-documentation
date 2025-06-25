1863cb1560b0387faffc01f16cff2bf61c615080
asrithnaverse <asrithnaverse@gmail.com>
2025-06-25 15:48:20 +0530

    Create test4.cpp

---
+++ TESTS/test4.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    cout << "X= ";
+    cin >> x;
+    if(x ==1){
+        cout << "Monday";
+    } 
+    if(x ==2){
+        cout << "Tuesday";
+    }
+    if(x==3){
+        cout << "Wednesday";
+    }
+    if(x==4){
+        cout << "Thursday";
+    }
+    if(x==5){
+        cout << "Friday";
+    }
+    if(x==6){
+        cout << "Saturday";
+    }
+    if(x==7){
+        cout << "Sunday";
+    }
+}
