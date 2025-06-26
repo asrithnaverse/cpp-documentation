32766c655a72de2548e4f0d65a4e0813637352ad
asrithnaverse <asrithnaverse@gmail.com>
2025-06-26 12:48:33 +0530

    Create thirteenth.cpp

---
+++ Basics/thirteenth.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[5];
+    cout << "The number is : ";
+    cin >> arr[0] >>arr[1] >> arr[2] >> arr[3] >> arr[4] ;
+    arr[4] += 198;
+    cout << arr[4];
+}
