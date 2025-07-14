1c849b7b2d08701205de5353b8ac1c254dc8006f
asrithnaverse <asrithnaverse@gmail.com>
2025-07-14 15:03:11 +0530

    Create twentisecond.cpp

---
+++ Basics/twentisecond.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum(int x, int y){
+    int sum3= x+y;
+    return sum3;
+}
+int main(){
+    int a; int b;
+    cout << "a is;";
+    cin >> a;
+    cout << "b is:";
+    cin >> b;
+    int c= sum(a,b);
+    cout << c;
+}
+    
