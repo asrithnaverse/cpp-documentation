f07de23965ee357d8cada01a103774cb935c2860
asrithnaverse <asrithnaverse@gmail.com>
2025-07-14 14:57:40 +0530

    Create twentifirst.cpp

---
+++ Basics/twentifirst.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printsum(int x,int y){
+    cout << "the sum is:" << x+y;
+}
+int main(){
+    int a; int b;
+    cout << "a is : " ;
+    cin >> a; 
+    cout << "b is:";
+    cin >> b;
+    printsum(a,b);
+}
