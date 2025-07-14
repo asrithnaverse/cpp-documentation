acdada022ad16f898f81e523576a390ad76341a1
asrithnaverse <asrithnaverse@gmail.com>
2025-07-14 14:39:32 +0530

    Create twentieth.cpp

---
+++ Basics/twentieth.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    void printName(string name){
+        cout << "hey " << name << "\n" ;
+    }
+int main(){
+    string naam;
+    cout << "your name ";
+    cin >> naam;
+    printName(naam);
+    string peeru;
+    cout << "your name is ";
+    cin >> peeru;
+    printName(peeru);
+}
