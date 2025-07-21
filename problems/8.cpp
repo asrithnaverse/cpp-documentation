2bbf5015222f25cefcc468ea3d54d6c267107a53
asrithnaverse <asrithnaverse@gmail.com>
2025-07-21 12:40:46 +0530

    Create 8.cpp

---
+++ problems/8.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int p,t;
+    float r;
+    cout << "principal= ";
+    cin >> p;
+    cout << "time= ";
+    cin >> t;
+    cout << "rate of interest= ";
+    cin >> r;
+    int s;
+    s= (p*t*r)/100;
+    cout << "simple interest = " << s;
+}
+    
