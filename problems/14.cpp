bcc6f5fc9de3cdab65c71746088c5f4d910e12f4
asrithnaverse <asrithnaverse@gmail.com>
2025-07-21 13:17:50 +0530

    Create 14.cpp

---
+++ problems/14.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float f,c;
+    cout << "celsius= ";
+    cin >> c;
+    f = (c*(9.0/5.0))+32;
+    cout << "fahreinheit= "<< f;
+}
+    
