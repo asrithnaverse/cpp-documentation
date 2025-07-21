a770556c4d3b3767508b7a32931dd6e3f379fdca
asrithnaverse <asrithnaverse@gmail.com>
2025-07-21 13:01:42 +0530

    Create 12.cpp

---
+++ problems/12.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x,y;
+    cout << "base = ";
+    cin >> x;
+    cout << "height = ";
+    cin >> y;
+    cout << "area of triangle= " << (x*y)/2;
+}
