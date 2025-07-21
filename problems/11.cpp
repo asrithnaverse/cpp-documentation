13d431786b17bdcf3d660be69e26c726f2ee7224
asrithnaverse <asrithnaverse@gmail.com>
2025-07-21 12:58:30 +0530

    Create 11.cpp

---
+++ problems/11.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float x,y;
+    cout <<"length= ";
+    cin >>x;
+    cout << "breadth= ";
+    cin >> y;
+    cout << "area = "<< x*y;
+}
