afbd1cf2836b3a72a2c6777df8b35491b7e78e56
asrithnaverse <asrithnaverse@gmail.com>
2025-07-21 12:55:10 +0530

    Create 10.cpp

---
+++ problems/10.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float r;
+    float pi;
+    cout << "radius= ";
+    cin >> r;
+    cout << "pi= ";
+    cin >> pi;
+    cout << "area = " << pi*pow(r, 2);
+}
