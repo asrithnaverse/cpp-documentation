dd8e443b66b29d7bde1b2635665f62b25f768903
asrithnaverse <asrithnaverse@gmail.com>
2025-07-21 12:23:38 +0530

    Create 5.cpp

---
+++ problems/5.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x,y;
+    cout << "x=";
+    cin >> x;
+    cout << "y=";
+    cin >> y;
+    cout << "the product is=" << x*y;
+}
