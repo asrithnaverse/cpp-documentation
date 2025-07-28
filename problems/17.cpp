38e3843f9bb5e0ef43df8194a40b752dac31271b
asrithnaverse <asrithnaverse@gmail.com>
2025-07-28 12:21:40 +0530

    Create 17.cpp

---
+++ problems/17.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=5;
+    int b=10;
+    cout << "before swapping a= " << a << " " << "b= " << b << "\n";
+    int temp=a;
+    a=b;
+    b=temp;
+    cout << "after swapping a= " << a << " " << "b= " << b;
+}
