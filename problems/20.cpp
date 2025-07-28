853444dfc85a88d47b02a4e1ba460b080b88c9fe
asrithnaverse <asrithnaverse@gmail.com>
2025-07-28 12:55:19 +0530

    Create 20.cpp

---
+++ problems/20.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cout << "a= " ;
+    cin >> a;
+    if(a<0){
+        cout << "negative number";
+    }
+    else if(a==0){
+        cout << "zero";
+    }
+    else{
+        cout << "postive";
+    }
+}
