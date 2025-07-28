4b89a5d8e7f91dc42b145a00c610868638b2454f
asrithnaverse <asrithnaverse@gmail.com>
2025-07-28 13:11:53 +0530

    Create 22.cpp

---
+++ problems/22.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout << "a= ";
+    cin >> a;
+    cout << "b= ";
+    cin >> b;
+    cout << "c= ";
+    cin >> c;
+    if(a>b){
+        if(a>c){
+            cout << a;
+        }
+        else{
+            cout << c;
+        }
+    }
+    else{
+        if(b>c){
+            cout << b;
+        }
+            else{
+                cout << c;
+            }
+        
+    }
+    }
+        
+    
