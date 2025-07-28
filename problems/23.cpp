7f3ac8d2e676f9ca7a64cb5981838785f88869c1
asrithnaverse <asrithnaverse@gmail.com>
2025-07-28 13:16:08 +0530

    Create 23.cpp

---
+++ problems/23.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cout << "a= ";
+    cin >> a;
+    if((a%4)==0){
+        cout << "leap year";
+    }
+    else{
+        cout << "not a leap  year";
+    }
+}
