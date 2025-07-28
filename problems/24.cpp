10dea4f92e1613a15db947f8db12997caee45f56
asrithnaverse <asrithnaverse@gmail.com>
2025-07-28 13:24:05 +0530

    Create 24.cpp

---
+++ problems/24.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char letter;
+    cout << "letter= ";
+    cin >> letter;
+    if(letter=='a'|| letter=='e' || letter=='i'|| letter=='o'|| letter=='u'|| letter=='A'|| letter=='E'||letter=='I'||letter=='O'||letter=='U'){
+        cout << "vowel";
+    }
+    else{
+        cout << "not a vowel";
+    } 
+}
